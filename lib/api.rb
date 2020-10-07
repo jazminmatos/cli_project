120d4a93af692b263d1f22e52fb683674169cd3d
jazminmatos <jmatos1993@gmail.com>
2020-10-07 08:54:51 -0400

    url updates

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,13 @@
 #handle all of our API requests (sending GET requests)
 
 class API
+    def self.pokemon_info(pokemon)
 
+    end
 end
 
-# Retrieve pokemon info: https://pokeapi.co/api/v2/pokemon/{id or name}/
+# list of 20 pokemon: https://pokeapi.co/api/v2/pokemon/ (paginated page)
+# list of original 151 pokemon: https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151
+    #iterate over the list pokemons above and list them
+#Ask user to select a number to learn more a pokemon's abilities and 
+    # Retrieve pokemon info: https://pokeapi.co/api/v2/pokemon/{id or name}/
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,6 +2,21 @@
 #contain all of our gets & puts
 #control the flow of our program
 
-class CLI
+#  REQUIREMENTS
+# Have a CLI for interfacing w/ the application
+# Pull data from an external source
+# Implement both list and detail views. The data provided must go at least one level deep.
+# The application = DRY
 
+class CLI
+    def start
+        puts ""
+        puts "Welcome to the Pokemon Database!"
+        puts ""
+        puts "Search for Pokemon by entering a Pokemon's name"
+        puts ""
+        @pokemon = gets.chomp.downcase
+        puts ""
+        API.pokemon_info(@pokemon)
+    end
 end
